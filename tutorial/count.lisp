099638f0140b31d1e4673c534bce0d4932f8f679
David Reitter <david.reitter@gmail.com>
2010-07-28 11:12:25 -0400

    fix Lisp style.  fix comments.  add  `speak' rule

--- tutorial/count.lisp
+++ tutorial/count.lisp
@@ -5,6 +5,7 @@
 (require "act-up" "../act-up.lisp")
 (use-package :act-up)
 
+;; ACT-R parameters
 (setq *lf* .05)
 (setq *rt* -1)
 
@@ -14,21 +15,26 @@
 
 ;;;; Committing chunks to memory
 
-(learn-chunk (make-count-order :name 'a :first 1 :second 2))
-(learn-chunk (make-count-order :name 'b :first 2 :second 3))
-(learn-chunk (make-count-order :name 'c :first 3 :second 4))
-(learn-chunk (make-count-order :name 'd :first 4 :second 5))
-(learn-chunk (make-count-order :name 'e :first 5 :second 6))
+(learn-chunk (make-count-order :name 'one-two :first 1 :second 2))
+(learn-chunk (make-count-order :name 'two-three :first 2 :second 3))
+(learn-chunk (make-count-order :name 'three-four :first 3 :second 4))
+(learn-chunk (make-count-order :name 'four-five :first 4 :second 5))
+(learn-chunk (make-count-order :name 'five-six :first 5 :second 6))
 
 ;;;; Defining procedural rule
 
 (defrule count-model (arg1 arg2) 
-  "arg1 is the starting point and arg2 is the ending point"
-  "each increment is 1 unit"
-  (print arg1)
+  "Count from ARG1 to ARG1.
+ARG1 is the starting point and ARG2 is the ending point.
+Each increment is 1 unit."
+  (speak arg1)
   (if (not (eq arg1 arg2))
       (let ((p (retrieve-chunk (list :chunk-type 'count-order 
 				     :first arg1))))
-	(progn
-	  (setq arg1 (count-order-second p))
-	  (count-model arg1 arg2)))))
+	(if p
+	    (count-model (count-order-second p) arg2)))))
+
+(defrule speak (number)
+  "Say number."
+  (print number)
+  (pass-time 0.3))
