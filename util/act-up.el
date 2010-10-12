35eac45529991656f8e0f6bd822b45bd52003a97
David Reitter <david.reitter@gmail.com>
2010-10-12 14:26:54 -0400

    provide documentation lookup function

--- util/act-up.el
+++ util/act-up.el
@@ -20,21 +20,58 @@
 ;; CODE
 
 ;; extract these with "make symbols" in doc/  (ACT-UP distribution):
-
-(defvar ACT-UP-SYMS '(*LE* *BLL* *OL* *PROCEDURE-COMPILATION* PASS-TIME BEST-CHUNK *EPL* REQUEST-BEST-CHUNK META-PROCESS-NAME MODULE-BUSY-P DEBUG-DETAIL* STOP-ACTUP-TIME LEARN-CHUNK RESET-MODULE DEFPROC DEBUG-CLEAR *ASSOCIATIVE-LEARNING* *DEBUG-TO-LOG* *MS* DEBUG-LOG *ALPHA* DEBUG-DETAIL SET-DM-TOTAL-PRESENTATIONS RESET-SJI-FCT *ALL* *IU* *EGS* *UL* *DAT* MAKE-CHUNK* SHOW-CHUNKS SET-BASE-LEVELS-FCT RECEIVE *MP* REQUEST BLEND SET-BASE-LEVEL-FCT PC *BLC* *CURRENT-ACTUP-META-PROCESS* EXPLAIN-ACTIVATION WITH-CURRENT-MODEL *MAXIMUM-ASSOCIATIVE-STRENGTH* MODEL-CHUNKS *UT* *LF* *AU-RFR* CURRENT-MODEL *DETAILED* *DEBUG* WAIT-FOR-MODEL DEBUG-GREP *WARNING* MAKE-MODEL SHOW-UTILITIES TERMINATE-REQUEST REQUEST-FILTER-CHUNKS RETRIEVE-CHUNK RESPONSE-AVAILABLE-P REQUEST-RETRIEVE-CHUNK SET-SIMILARITIES-FCT WAIT-FOR-RESPONSE *RT* SET-CURRENT-MODEL BLEND-RETRIEVE-CHUNK MAKE-META-PROCESS DEFINE-CHUNK-TYPE WAIT-FOR-MODULE *MAS* *AU-RPPS* *CRITICAL* MAKE-CHUNK RESET-MP RESET-MODEL CHUNK-TYPE *INFORMATIONAL* META-PROCESS FLUSH-PROCEDURE-QUEUE *NU* ADD-SJI-FCT ASSIGN-REWARD* *PAS* ASSIGN-REWARD *ANS* NON-NIL ACTUP-CHUNK DEFINE-SLOTS CHUNK-NAME ACTUP-TIME FILTER-CHUNKS *MD* MODEL-NAME)) 
+;; must change to lower case.
+(setq ACT-UP-SYMS (mapcar (lambda (x) 
+			      (format "%s" x))
+			      '(*le* *bll* *ol* *procedure-compilation* pass-time best-chunk *epl* request-best-chunk meta-process-name module-busy-p debug-detail* stop-actup-time learn-chunk reset-module defproc debug-clear *associative-learning* *debug-to-log* *ms* debug-log *alpha* debug-detail set-dm-total-presentations reset-sji-fct *all* *iu* *egs* *ul* *dat* make-chunk* show-chunks set-base-levels-fct receive *mp* request blend set-base-level-fct pc *blc* *current-actup-meta-process* explain-activation with-current-model *maximum-associative-strength* model-chunks *ut* *lf* *au-rfr* current-model *detailed* *debug* wait-for-model debug-grep *warning* make-model show-utilities terminate-request request-filter-chunks retrieve-chunk response-available-p request-retrieve-chunk set-similarities-fct wait-for-response *rt* set-current-model blend-retrieve-chunk make-meta-process define-chunk-type wait-for-module *mas* *au-rpps* *critical* make-chunk reset-mp reset-model chunk-type *informational* meta-process flush-procedure-queue *nu* add-sji-fct assign-reward* *pas* assign-reward *ans* non-nil actup-chunk define-slots chunk-name actup-time filter-chunks *md* model-name)))
 
 
 (define-derived-mode act-up-mode lisp-mode "ACT-UP"
   "Major mode for editing ACT-UP cognitive models.
+Provides a binding for C-c C-d h to look up symbols
+in the Common Lisp HyperSpec or in the ACT-UP documentation.
 \\{act-up-mode-map}"
-  (show-paren-mode 1))
-
+  (show-paren-mode 1)
+  (mapcar (lambda (s)
+	    (intern s common-lisp-hyperspec-symbols))
+	  ACT-UP-SYMS)
+  (setq slime-documentation-lookup-function #'act-up-slime-hyperspec-lookup))
 
 (font-lock-add-keywords 'act-up-mode 
 			(mapcar (lambda (s)
-				  (cons (regexp-quote (format "%s" s))
+				  (cons (regexp-quote s)
 					'font-lock-keyword-face))
 				ACT-UP-SYMS))
+
+(defun lookup-act-up-symbol (string)
+  (interactive "MLook up symbol (ACT-UP): ")
+  (browse-url (format "http://act-up.psy.cmu.edu/ACT-UP.html#%s" string)))
+
+(defvar act-up-common-lisp-hyperspec-symbols nil)
+(defun act-up-slime-hyperspec-lookup (symbol-name)
+  "Look up a symbol in the Common Lisp HyperSpec or ACT-UP documentation.
+A wrapper for `hyperspec-lookup'"
+  (interactive (list (let* ((symbol-at-point (slime-symbol-at-point))
+			    (completion-ignore-case t)
+                            (stripped-symbol 
+                             (and symbol-at-point
+                                  (downcase
+                                   (common-lisp-hyperspec-strip-cl-package 
+                                    symbol-at-point)))))
+                       (if (and stripped-symbol
+				(or 
+				 (member-ignore-case stripped-symbol ACT-UP-SYMS)
+				 (intern-soft stripped-symbol
+					      common-lisp-hyperspec-symbols)))
+                           stripped-symbol
+			 (completing-read
+			  "Look up symbol (Common Lisp or ACT-UP): "
+			  common-lisp-hyperspec-symbols nil ;; #'boundp
+			  t stripped-symbol
+			  'common-lisp-hyperspec-history)))))
+  (if (member-ignore-case symbol-name ACT-UP-SYMS)
+      (lookup-act-up-symbol symbol-name)
+    (hyperspec-lookup symbol-name)))
 
 
 (provide 'act-up)
